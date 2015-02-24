adc00b16ec8d144482b69a0d47cce28c4deb8adf
Greg Hartman <ghartman@google.com>
2015-02-23 20:04:52 -0800

    Add host build rules for sgdisk
    Change-Id: Ided9a1329ade081228101d51812242dc4d686b87

--- Android.mk
+++ Android.mk
@@ -1,9 +1,6 @@
 LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
 
-LOCAL_CPP_EXTENSION := .cc
-
-LOCAL_SRC_FILES := \
+sgdisk_src_files := \
     sgdisk.cc \
     gptcl.cc \
     crc32.cc \
@@ -21,9 +18,30 @@ LOCAL_SRC_FILES := \
     diskio-unix.cc \
     android_popt.cc \
 
+include $(CLEAR_VARS)
+
+LOCAL_CPP_EXTENSION := .cc
+
+LOCAL_SRC_FILES := $(sgdisk_src_files)
+
 LOCAL_SHARED_LIBRARIES := libext2_uuid
 
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := sgdisk
 
 include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+
+LOCAL_CPP_EXTENSION := .cc
+
+LOCAL_SRC_FILES := $(sgdisk_src_files)
+
+LOCAL_CFLAGS := -D__ANDROID__
+LOCAL_SHARED_LIBRARIES := libext2_uuid_host
+
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE := sgdisk_host
+LOCAL_MODULE_STEM := sgdisk
+
+include $(BUILD_HOST_EXECUTABLE)
