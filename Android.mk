6d73fc63a4d9b3454ddfeb6a95387fce4ff95f85
Greg Hartman <ghartman@google.com>
2015-02-24 18:27:10 +0000

    Revert "Add host build rules for sgdisk"
    This reverts commit adc00b16ec8d144482b69a0d47cce28c4deb8adf.
    
    Change-Id: I12ff87b2d042494e6ad20ffa6d7626c143224f46

--- Android.mk
+++ Android.mk
@@ -1,6 +1,9 @@
 LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
 
-sgdisk_src_files := \
+LOCAL_CPP_EXTENSION := .cc
+
+LOCAL_SRC_FILES := \
     sgdisk.cc \
     gptcl.cc \
     crc32.cc \
@@ -18,30 +21,9 @@ sgdisk_src_files := \
     diskio-unix.cc \
     android_popt.cc \
 
-include $(CLEAR_VARS)
-
-LOCAL_CPP_EXTENSION := .cc
-
-LOCAL_SRC_FILES := $(sgdisk_src_files)
-
 LOCAL_SHARED_LIBRARIES := libext2_uuid
 
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := sgdisk
 
 include $(BUILD_EXECUTABLE)
-
-include $(CLEAR_VARS)
-
-LOCAL_CPP_EXTENSION := .cc
-
-LOCAL_SRC_FILES := $(sgdisk_src_files)
-
-LOCAL_CFLAGS := -D__ANDROID__
-LOCAL_SHARED_LIBRARIES := libext2_uuid_host
-
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE := sgdisk_host
-LOCAL_MODULE_STEM := sgdisk
-
-include $(BUILD_HOST_EXECUTABLE)
